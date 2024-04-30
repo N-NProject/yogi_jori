3b84aafd4ad72f854e6c8a5e00a2b6b8d1a35a8e
Kimyebin00 <kimyeobin12@gmail.com>
2024-04-30 15:58:43 +0900

    refactor: css 다듬기

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,8 +3,11 @@ import PostPreview from "@/components/PostPreview"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <PostPreview/>
+    <main className="flex min-h-screen flex-col justify-between">
+      <div className="flex flex-row min-h-screen justify-center p-24">
+        <PostPreview/>
+        <PostPreview/>
+      </div>
     </main>
   );
 }
--- my-app/src/components/PostPreview/index.tsx
+++ my-app/src/components/PostPreview/index.tsx
@@ -8,7 +8,7 @@ import PersonnelIcon from "@/assets/previewImages/PersonnelIcon.png";
 const Comment = () => {
   return (
     <>
-      <div className="flex flex-row w-fit bg-white rounded-2xl p-4">
+      <div className="flex flex-row w-1/3 h-32 bg-white rounded-3xl p-3 border-solid border-2 mx-5">
         <Image
           src={StudyInCafeImage}
           alt="Study In Cafe Preview Image"
@@ -18,19 +18,19 @@ const Comment = () => {
         />
         <div className="ml-3">
           <p className="w-fit bg-rose-100 px-2.5 py-0.5 rounded-xl text-xs mb-1.5">카공</p>
-          <p className="text-lg font-semibold	mb-5">한국공학대 근처에서 카공할 사람!</p>
+          <p className="text-base font-semibold	mb-8">한국공학대 근처에서 카공할 사람!</p>
           <div className="flex flex-row">
-            <div className="flex flex-row mr-3">
+            <div className="flex flex-row mr-3 items-center">
               <Image
                 src={LocationIcon}
                 alt="Location Icon Image"
-                width={20}
-                height={20}
+                width={18}
+                height={18}
                 priority
               />
-              <span className="text-sm ml-1 text-gray-500">경기도 시흥시</span>
+              <span className="text-xs ml-1 text-gray-500">경기도 시흥시</span>
             </div>
-            <div className="flex flex-row mr-3">
+            <div className="flex flex-row mr-3 items-center">
               <Image
                 src={TimeIcon}
                 alt="Time Icon Image"
@@ -38,9 +38,9 @@ const Comment = () => {
                 height={16}
                 priority
               />
-              <span className="text-sm ml-2 text-gray-500">4/19(금) 14:00</span>
+              <span className="text-xs ml-2 text-gray-500	">4/19(금) 14:00</span>
             </div>
-            <div className="flex flex-row">
+            <div className="flex flex-row items-center">
               <Image
                 src={PersonnelIcon}
                 alt="Personnel Icon Image"
@@ -48,7 +48,7 @@ const Comment = () => {
                 height={22}
                 priority
               />
-              <span className="text-sm ml-1 text-gray-500">1/4</span>
+              <span className="text-xs ml-1 text-gray-500">1/4</span>
             </div>
           </div>
         </div>
