b4e9fbdf869115af747408246c65ba6da0884905
jaemin <44522204+penloo@users.noreply.github.com>
2024-05-14 23:30:08 +0900

    Update layout.tsx

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex flex-row">
-        {/* <NavBar /> */}
+        <NavBar />
         <main className="w-full">{children}</main>
       </body>
     </html>
