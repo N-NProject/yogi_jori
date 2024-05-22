223c5e5082415c661778f3adbb5909a4a7cc8f44
Kimyebin00 <kimyeobin12@gmail.com>
2024-05-22 11:04:24 +0900

    feat: 게시글 작성 페이지 반응형 UI 적용

--- my-app/src/app/write/page.tsx
+++ my-app/src/app/write/page.tsx
@@ -18,9 +18,9 @@ const Write = () => {
   return (
     <div className="flex flex-col h-full items-center justify-center">
       <h1 className="text-darkpink font-semibold text-2xl">모집하기</h1>
-      <div className="flex flex-col items-center w-[30rem] space-y-4 my-10">
+      <div className="flex flex-col items-center md:w-[30rem] w-[22rem] space-y-4 my-10">
         <input type="text" name="title" placeholder="제목을 입력해주세요." className="placeholder:text-zinc-500 border-[1.5px] border-solid border-pink outline-darkpink rounded-[3px] h-11 w-full px-4 py-2.5 font-semibold text-sm"/>
-        <div className="flex justify-between w-full relative">
+        <div className="flex md:flex-row flex-col justify-between w-full relative">
           <DatePicker
             showTimeInput
             dateFormat='yyyy / MM / dd  HH:mm' // 날짜 형태
@@ -33,21 +33,24 @@ const Write = () => {
             className="placeholder:text-zinc-500 border-[1.5px] border-solid border-pink outline-darkpink rounded-[3px] h-11 w-full px-4 py-2.5 font-semibold text-sm"
             calendarClassName="bg-pink"
           />
-          <input type='time' className='placeholder:text-zinc-500 border-[1.5px] border-solid border-pink outline-darkpink rounded-[3px] h-11 w-36 absolute right-[7rem] px-4 py-2.5 font-semibold text-sm text-zinc-500 cursor-pointer'/>
-          <div className='bg-white absolute border-[1.5px] border-solid border-pink rounded-[3px] h-50 right-0 z-10 max-w-[10rem] cursor-pointer'>
-            <ul onClick={() => {setView(!view)}} className='overflow-auto w-24 px-4 py-2.5 font-semibold text-sm text-zinc-500'>
-              {person}{person === undefined ? '인원 선택' : "명"}
-              {/* {view ? '⌃' : '⌄'} */}
-              {view && 
-                <>
-                  {personItems.map((item, index) => (
-                    <li key={index} onClick={() => setPerson(item)} className='hover:text-darkpink w-100 flex bg-white h-8 items-center justify-center'>{item}</li>
-                  ))}
-                </>
-                
-              }
-            </ul>
+          <div className='flex md:flex-row justify-between md:mt-0 mt-4'>
+            <input type='time' className='placeholder:text-zinc-500 border-[1.5px] border-solid border-pink outline-darkpink rounded-[3px] h-11 w-36 md:absolute right-[7rem] px-4 py-2.5 font-semibold text-sm text-zinc-500 cursor-pointer'/>
+            <div className='bg-white md:absolute border-[1.5px] border-solid border-pink rounded-[3px] h-50 right-0 z-10 max-w-[10rem] cursor-pointer'>
+              <ul onClick={() => {setView(!view)}} className='overflow-auto w-24 px-4 py-2.5 font-semibold text-sm text-zinc-500'>
+                {person}{person === undefined ? '인원 선택' : "명"}
+                {/* {view ? '⌃' : '⌄'} */}
+                {view && 
+                  <>
+                    {personItems.map((item, index) => (
+                      <li key={index} onClick={() => setPerson(item)} className='hover:text-darkpink w-100 flex bg-white h-8 items-center justify-center'>{item}</li>
+                    ))}
+                  </>
+                  
+                }
+              </ul>
+            </div>
           </div>
+          
 
         </div>
         <div className="flex justify-between w-full">
@@ -60,7 +63,7 @@ const Write = () => {
         <textarea name="description" placeholder="상세 내용을 입력해주세요." rows="12" className="placeholder:text-zinc-500 border-[1.5px] border-solid border-pink outline-darkpink rounded-[3px] h-70 w-full px-4 py-2.5 font-semibold text-sm"/>
       </div>
       <Link href="/post">
-        <button className="w-[30rem] h-fit bg-darkpink text-md font-semibold text-white rounded-lg py-2 px-20 cursor-pointer">완료</button>  
+        <button className="md:w-[30rem] w-[22rem] h-fit bg-darkpink text-md font-semibold text-white rounded-lg py-2 px-20 cursor-pointer">완료</button>  
       </Link>
     </div>
   );
