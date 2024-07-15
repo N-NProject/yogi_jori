fd4a235d60ba07042b4ad6c0918688eaf2239e18
Kimyebin00 <kimyeobin12@gmail.com>
2024-07-16 02:47:45 +0900

    feat: 게시글 수정 모달창 구현 및 모달창 내 기본 입력값 설정 완료

--- my-app/src/constants/loginToken.ts
+++ my-app/src/constants/loginToken.ts
@@ -1,2 +1,2 @@
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImlhdCI6MTcyMDUzMjY1NCwiZXhwIjoxNzIwNTM2MjU0fQ.2vDMafkS18d2d9BaPFIUe6waX6dDHq433FT3LjzpqNQ";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImlhdCI6MTcyMTA2MzIyMywiZXhwIjoxNzIxMDY2ODIzfQ.aDSSDipuugSDZhb2_cpH9i9drFQ1fPKEN_0IiVAtz_g";
 export default token;
