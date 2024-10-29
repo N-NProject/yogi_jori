d971c288bcbb187d9b6c0296e980cfd728cd417a
Kimyebin00 <kimyeobin12@gmail.com>
2024-10-29 19:13:39 +0900

    fix: 타입 수정

--- my-app/src/components/PostPreview.tsx
+++ my-app/src/components/PostPreview.tsx
@@ -32,7 +32,7 @@ const tagColors: { [key: string]: string } = {
   보드게임: "bg-yellow-100",
 };
 
-const categoryImages: { [key: string]: typeof CoffeeChatImage } = {
+const categoryImages: { [key: string]: string } = {
   커피챗: CoffeeChatImage,
   카공: StudyInCafeImage,
   보드게임: BoardgameImage,
